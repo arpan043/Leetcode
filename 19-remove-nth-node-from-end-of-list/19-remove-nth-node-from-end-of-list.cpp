5c3423be4a3861e40b18d57d19873e4c02550944
arpan043 <38400610+arpan043@users.noreply.github.com>
2022-03-31 21:58:00 +0530

    Time: 7 ms (49.70%), Space: 10.6 MB (75.13%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -14,12 +14,16 @@ public:
         ListNode *start=new ListNode(); 
         start->next=head;
         ListNode *slow=start,*fast=start;
-        for(int i=0;i<n;i++)
-            fast=fast->next; 
-        
-        while(fast->next!=nullptr){  
-            fast=fast->next;
-            slow=slow->next;
+        int count = n;
+        while(count > 0)
+        {
+            fast = fast->next;
+            count--;
+        }
+        while(fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next;
         }
         ListNode *del=slow->next; 
         slow->next=slow->next->next; 
